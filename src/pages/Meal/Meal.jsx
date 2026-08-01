5c673d5aeeebd9cbcf1c7f6bd284cc59f8b333b1
janaescustom <janaescustom@gmail.com>
2026-08-01 16:02:24 +0200

    updating Meal

--- src/pages/Meal/Meal.jsx
+++ src/pages/Meal/Meal.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./Meal.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Meal = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+
   const [recipeTitle, setRecipeTitle] = useState("");
   const [recipeImage, setRecipeImage] = useState("");
   const [recipeIngredients, setRecipeIngredients] = useState([]);
   const [measurements, setMeasurements] = useState([]);
   const [recipeInstructions, setRecipeInstructions] = useState("");
+  const [recipeData, setRecipeData] = useState({
+    strMeal: "",
+    strMealThumb: "",
+    strInstructions: "",
+  });
 
   useEffect(() => {
     async function fetchMealDetails() {
@@ -20,8 +27,13 @@ const Meal = () => {
         const { data } = await axios.get(
           `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
         );
+        setRecipeData(data.meals[0]);
         setRecipeTitle(data.meals[0].strMeal);
-        setRecipeImage(data.meals[0].strMealThumb);
+
+        setRecipeImage(
+          `${data.meals[0].strMealThumb}/medium` ||
+            "https://tse2.mm.bing.net/th/id/OIP.03VsueF0bkEMTgH1hwODLAHaE7?pid=ImgDet&w=208&h=138&c=7&dpr=1.3&o=7&rm=3",
+        );
         for (let i = 1; i <= 20; i++) {
           const ingredient = data.meals[0][`strIngredient${i}`];
           const measurement = data.meals[0][`strMeasure${i}`];
@@ -40,7 +52,6 @@ const Meal = () => {
       }
     }
     fetchMealDetails();
-    // console.log('Meal name:', recipeTitle);
   }, [id]);
 
   return (
@@ -49,37 +60,36 @@ const Meal = () => {
         <div className="recipes__container">
           <div className="row">
             <div className="recipe__selected--top">
-              <div className="back__btn">
-                <Link to="/results" className="recipe__link">
+              <div className="back__btn" onClick={() => navigate(-1)}>
                 <FontAwesomeIcon icon="arrow-left" className="recipe__icon" />
-                <h3 className="recipe__back">All Meals</h3>
-                </Link>
+                <h3 className="recipe__back">Back</h3>
               </div>
               <div className="bookmark__container">
                 <FontAwesomeIcon icon="bookmark" />
               </div>
             </div>
-                <img
-                  src={recipeImage}
-                  alt={recipeTitle}
-                  className="meal__image"
-                />
-
-            <h1 className="meal__title">{recipeTitle}</h1>
-
-            <div className="ingredients__table">
-              <h2>Ingredients</h2>
-              <ul className="ingredient__list">
-                {recipeIngredients.map((ingredient, index) => (
-                  <li key={index}>
-                    {measurements[index]} {ingredient}
-                  </li>
-                ))}
-              </ul>
+            <div className="image__container">
+              <img
+                src={recipeImage}
+                alt={recipeTitle}
+                className="meal__image"
+              />
             </div>
-
-            <h2>Instructions</h2>
-            <p className="instructions">{recipeInstructions}</p>
+            <div className="recipe__selected--content">
+              <h1 className="recipe__title">{recipeTitle}</h1>
+              <div className="ingredients__table">
+                <h2>Ingredients</h2>
+                <ul className="ingredient__list">
+                  {recipeIngredients.map((ingredient, index) => (
+                    <li key={index}>
+                      {measurements[index]} {ingredient}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <h2>Instructions</h2>
+              <p className="instructions">{recipeInstructions}</p>
+            </div>
           </div>
         </div>
       </main>
